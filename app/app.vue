93c3c086428faae2db29378c9727944b15a8efde
pejoal <pejoal.official@gmail.com>
2025-11-07 23:52:45 +0100

    refactor: Remove GitHub Actions workflow for deployment to GitHub Pages

--- app/app.vue
+++ app/app.vue
@@ -3,5 +3,6 @@
     <NuxtRouteAnnouncer />
     <!-- <NuxtWelcome /> -->
     <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <h1 class="text-2xl font-bold underline">Test</h1>
   </div>
 </template>
