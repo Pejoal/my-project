d45496eb3568f26ff7546cb60674ff0fc9ed3d22
pejoal <pejoal.official@gmail.com>
2025-11-07 23:12:25 +0100

    Add Tailwind CSS integration and update app layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  css: ['./app/assets/css/main.css'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/scripts',
-    '@nuxt/test-utils',
-    '@nuxt/ui'
-  ]
-})
+  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/image', '@nuxt/scripts', '@nuxt/test-utils', '@nuxt/ui'],
+});
