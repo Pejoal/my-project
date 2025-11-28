c85fd5bb9b556aead7872876c6fc99178d5ad238
pejoal <pejoal.official@gmail.com>
2025-11-28 18:21:20 +0100

    fix: Update app icons to use image URLs and add 'Connect 2048' game

--- app/composables/useApp.js
+++ app/composables/useApp.js
@@ -109,7 +109,7 @@ export const useAppData = () => {
       iosId: 'apprendre-le-français/id6754800257',
       title: 'Learn French',
       description: 'Master French language with ease',
-      icon: '🇫🇷',
+      icon: 'https://github-production-user-asset-6210df.s3.amazonaws.com/105957829/520253810-9b65e8b8-af6f-4612-972d-9f577f733460.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20251128%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20251128T172057Z&X-Amz-Expires=300&X-Amz-Signature=b6b5cea7586d8ff87a51095fdb8f935d37669581695306781ae39b356059ff7f&X-Amz-SignedHeaders=host',
       category: 'other',
     },
     {
@@ -176,7 +176,7 @@ export const useAppData = () => {
       iosId: 'slides-puzzle/id6754782354',
       title: 'Slides Puzzle',
       description: 'Classic sliding puzzle game',
-      icon: '🧩',
+      icon: 'https://github-production-user-asset-6210df.s3.amazonaws.com/105957829/520252756-5aaa0bf5-5eb4-4074-8045-d1b40b03fb2a.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20251128%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20251128T171749Z&X-Amz-Expires=300&X-Amz-Signature=0be3386e5cf42d7979b7e620132de9f3baf134c1c57cc788b338ea6728138d21&X-Amz-SignedHeaders=host',
       category: 'game',
     },
     {
@@ -184,7 +184,15 @@ export const useAppData = () => {
       iosId: 'drop-2048-tile-merge-puzzle/id6755253445',
       title: 'Drop 2048',
       description: 'Merge tiles to reach 2048',
-      icon: '✨',
+      icon: 'https://github-production-user-asset-6210df.s3.amazonaws.com/105957829/520253388-78771fd1-2f5d-4b54-afc8-66b7bfb8e8d0.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20251128%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20251128T171940Z&X-Amz-Expires=300&X-Amz-Signature=4d20b01b46e51c34a2dde66cb0fd4273fb7883d934d3a1109ac76ff18ba11908&X-Amz-SignedHeaders=host',
+      category: 'game',
+    },
+    {
+      id: 'connect_2048',
+      iosId: null,
+      title: 'Connect 2048',
+      description: 'Connect & Chain tiles to reach 2048',
+      icon: 'https://github-production-user-asset-6210df.s3.amazonaws.com/105957829/520251913-0b9e4783-3377-4a19-989c-9b1d6c141178.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVCODYLSA53PQK4ZA%2F20251128%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20251128T171451Z&X-Amz-Expires=300&X-Amz-Signature=7b5eba23d5b0215652bb38e034246de607dc058503c718238dfd2e5661e19b3d&X-Amz-SignedHeaders=host',
       category: 'game',
     },
   ]);
